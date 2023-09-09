eddf0d421e871275c843f37811934420af607597
schabibi1 <arisagse@gmail.com>
2023-09-09 14:52:41 +0200

    add part-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,27 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import tailwind from "@astrojs/tailwind"
+import basicSsl from '@vitejs/plugin-basic-ssl'
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+    tailwind()
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+})
