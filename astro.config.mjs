e72e939fc001e87bba881ec71575490ba846d971
schabibi1 <arisagse@gmail.com>
2023-10-26 19:56:57 +0200

    add: Vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,31 @@
-import { defineConfig } from 'astro/config'
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
-import tailwind from "@astrojs/tailwind"
-import basicSsl from '@vitejs/plugin-basic-ssl'
-const env = loadEnv("", process.cwd(), 'STORYBLOK')
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import tailwind from "@astrojs/tailwind";
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import vercel from "@astrojs/vercel/serverless";
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        page: 'storyblok/Page',
-        config: 'storyblok/Config',
-        feature: 'storyblok/Feature',
-        grid: 'storyblok/Grid',
-        teaser: 'storyblok/Teaser',
-        hero: 'storyblok/Hero',
-        article: 'storyblok/Article',
-      },
-    }),
-    tailwind()
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: 'storyblok/Page',
+      config: 'storyblok/Config',
+      feature: 'storyblok/Feature',
+      grid: 'storyblok/Grid',
+      teaser: 'storyblok/Teaser',
+      hero: 'storyblok/Hero',
+      article: 'storyblok/Article'
+    }
+  }), tailwind()],
   vite: {
     plugins: [basicSsl()],
     server: {
-      https: true,
-    },
+      https: true
+    }
   },
-})
+  output: "server",
+  adapter: vercel()
+});
